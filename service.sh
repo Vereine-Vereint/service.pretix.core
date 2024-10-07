78862128151dd308ef0e8c4d711ca0165a5c99ad
Johannes Schmid <johannes@schmid-company.de>
2024-10-07 10:58:55 +0200

    added support for extensions to pretix

--- service.sh
+++ service.sh
@@ -11,24 +11,16 @@ cd $SERVICE_DIR
 # CORE
 source ./core/core.sh
 
-
 # VARIABLES
 set -o allexport
-# set variables for docker or other services here
+
 set +o allexport
 
 # COMMANDS
-
-# This is an example command that prints a message from the first argument
 commands+=([setup-cron]=":Setup Cronjob for Pretix")
 cmd_setup-cron() {
   echo "15,45 * * * * /usr/bin/docker exec pretix_pretix pretix cron"
 }
-
-# commands+=([run-compress]=":Setup Pretix - Compress")
-# cmd_run-compress() {
-#   docker compose exec pretix python /pretix/src/manage.py compress
-# }
 
 commands+=([exec]=":Execute a command in the pretix container")
 cmd_exec() {
@@ -36,6 +28,22 @@ cmd_exec() {
 }
 
 # ATTACHMENTS
+
+att_pull() {
+  if [[ -n "$PRETIX_EXTENSIONS" ]]; then
+    docker pull pretix/standalone:stable
+  fi
+}
+
+att_setup() {
+  if [[ -n "$PRETIX_EXTENSIONS" ]]; then
+    export PRETIX_SOURCE="build: generated"  
+    generate templates/Dockerfile generated/Dockerfile
+  else
+    export PRETIX_SOURCE="image: pretix/standalone:stable"
+  fi
+  generate templates/docker-compose.yml docker-compose.yml
+}
 
 att_post-setup() {
   # Setup Pretix - Compress
